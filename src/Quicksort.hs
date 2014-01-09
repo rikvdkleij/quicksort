39b5bd1c93455dadd8812650cb6d48bb8c461776
Rik van der Kleij <rikvdkleij@gmail.com>
2014-01-09 10:45:44 +0100

    Stylish haskell

--- src/Quicksort.hs
+++ src/Quicksort.hs
@@ -2,6 +2,6 @@ module Quicksort where
 
 quicksort :: (Ord t) => [t] -> [t]
 quicksort [] = []
-quicksort (x:xs) = quicksort smallerOrEqual ++ [x] ++ quicksort larger 
+quicksort (x:xs) = quicksort smallerOrEqual ++ [x] ++ quicksort larger
     where smallerOrEqual = [y | y <- xs, y <= x]
           larger = [y | y <- xs, y > x]
--- test/QuicksortSpec.hs
+++ test/QuicksortSpec.hs
@@ -9,7 +9,7 @@ import           Test.QuickCheck
 main :: IO ()
 main = hspec spec
 
-spec :: Spec 
+spec :: Spec
 spec =  do
   describe "quicksort" $ do
     it "sorts empty list" $ do
@@ -21,8 +21,8 @@ spec =  do
     it "sorts list of size 2" $ do
       quicksort [2,1] `shouldBe` ([1,2]::[Int])
 
-    it "sorts list of chars" $ do 
+    it "sorts list of chars" $ do
         quicksort "bca" `shouldBe` "abc"
-        
+
     it "returns same result if called twice" $ property $
        \xs -> (quicksort . quicksort) xs == (quicksort xs :: [Int])
